0d6a1110f5338d958368d7fe891cc06434587c83
vcwai <victorcwai@gmail.com>
2016-03-22 03:08:21 +0800

    Updated to filter MAX(Quantity) instead of Quantity

--- SQLquery.scala
+++ SQLquery.scala
@@ -12,12 +12,12 @@ object SQLquery {
       val strArrayRDD = strRDD.map(line => line.split("#")) //strArrayRDD is a matrix of n*4
       val intListRDD = strArrayRDD.map(x => List(x(1).toInt,x(2).toInt,x(3).toInt))
       val sortedIntListRDD = intListRDD.sortBy(x=>x(2),false)
-      val groupedRDD = sortedIntListRDD.groupBy(x=>x(1))
-      val filteredListRDD = groupedRDD.map(_._2.filter(x => x(2)>=lower_bound && x(2)<=upper_bound)).map(_.toList).filter(_.size>0)
+      val groupedRDD = sortedIntListRDD.groupBy(x=>x(1)).map(_._2.toList)
+      val filteredListRDD = groupedRDD.filter(x => x(0)(2)>=lower_bound && x(0)(2)<=upper_bound).filter(_.size>0)
       val lastRDD = filteredListRDD.map(x => x(0)).sortBy(x=>x(1)).sortBy(x=>x(0)).map(x =>{(x(0),x(1),x(2))})
       //filteredListRDD.collect().foreach(println)
+            
       lastRDD
-      
    }
 
   def main(args: Array[String]) {
@@ -27,11 +27,10 @@ object SQLquery {
     val sc = new SparkContext(conf)
 
     val Results = getResults(sc, args(1).toInt, args(2).toInt, args(0))
-    //val Results = getResults(sc, 60, 120, "/home/bigdata/Assignment2/Data/OrderDetails")
+    //val Results = getResults(sc, 60, 60,"/home/bigdata/Assignment2/Data/OrderDetails")
     Results.collect().foreach(println)
     println("Number of results: %s".format(Results.count()))
     
   }
 
 }
-
